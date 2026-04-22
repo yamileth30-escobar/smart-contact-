fbbfa571f131fef44fb69cc9ef81d91957ef0748
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-04-22 15:10:14 -0600

    Add files via upload

---
+++ EL/ContactoEL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContactos.EL
+{
+    public class ContactoEL
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public int CategoriaId { get; set; } 
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ EL/UsuarioEL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContactos.EL
+{
+    public class UsuarioEL
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
